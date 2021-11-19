7bcedb2ebb30663877a5ef16da04d358f7217d8b
Tudor Donca <tgdonca@gmail.com>
2021-11-19 11:23:05 -0800

    Adding hash table library starter files

---
+++ src/hash_table/hash_table.c
@@ -0,0 +1,12 @@
+
+// TODO: Remove explicit path dependency when switching to CMake
+#include "include/hash_table.h"
+
+// USE UTHASH library and just have this be a wrapper implementation
+// try to make it a little bit generic
+// ex: use HASH_ADD() instead of the specific HASH_ADD_INT()
+
+// Goal: handle ints, strings, and chars seamlessly
+
+
+// Followup will be implementing the hash table from scratch 
